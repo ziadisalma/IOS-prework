079f1e9b19d5cb00f19767c1cc203d627a232f8d
ziadisalma <ziadi22s@mtholyoke.edu>
2025-02-01 01:38:57 -0500

    Initial Commit

---
+++ IOS101-prework/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  IOS101-prework
+//
+//  Created by Salma Ziadi on 2/1/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+    }
+    func changeColor() -> UIColor{
+
+            let red = CGFloat.random(in: 0...1)
+            let green = CGFloat.random(in: 0...1)
+            let blue = CGFloat.random(in: 0...1)
+
+            return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+        }
+    
+}
+
